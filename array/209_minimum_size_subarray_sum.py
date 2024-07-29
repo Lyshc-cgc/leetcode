3b023aa2de3b7ea7babe2714cdbeb2daeff12d41
‘Lyshc’ <992697280@qq.com>
2024-07-29 21:57:26 +0800

    initial commit. 代码随想录数组部分第4,5节

---
+++ array/209_minimum_size_subarray_sum.py
@@ -0,0 +1,33 @@
+# 209. 长度最小的子数组
+# https://leetcode.cn/problems/minimum-size-subarray-sum/description/
+from typing import List
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        滑动窗口法
+        :param target:
+        :param nums:
+        :return:
+        """
+        start = 0  # 滑动窗口起始位置
+        min_size = 1000000  # 滑动窗口最小长度
+        window_sum = 0
+        for end in range(len(nums)):
+            window_sum += nums[end]
+            while window_sum >= target:  # 当window中各数之和大于等于target，记录目前窗口状态并则尝试缩小窗口
+                window_size = end - start + 1
+                if window_size < min_size:
+                    min_size = window_size
+                # 缩小窗口
+                window_sum -= nums[start]
+                start += 1
+        if min_size == 1000000:
+            return 0
+        return min_size
+
+
+if __name__ == '__main__':
+    nums = [2,3,1,2,4,3]
+    target = 7
+    print(Solution().minSubArrayLen(target, nums))
---
+++ array/977_squares_of_a_sorted_array.py
@@ -0,0 +1,27 @@
+# 977. 有序数组的平方
+# https://leetcode.cn/problems/squares-of-a-sorted-array/description/
+from typing import List
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        """
+        相向指针，由于数组是非递减的，那么平方最大值一定在"外围"的位置
+        :param nums:
+        :return:
+        """
+        left, right = 0, len(nums) - 1
+        res = []
+        while(left <= right):
+            num_left = nums[left] ** 2
+            num_right = nums[right] ** 2
+            if num_left <= num_right:  # 谁大就把谁放进去，再移动指针
+                res.append(num_right)
+                right -= 1
+            else:
+                res.append(num_left)
+                left += 1
+
+        return res[::-1]
+
+if __name__ == '__main__':
+    nums = [-7,-3,2,3,11]
+    print(Solution().sortedSquares(nums))
